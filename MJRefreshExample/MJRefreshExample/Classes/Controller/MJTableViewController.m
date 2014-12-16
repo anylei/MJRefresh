7f95b72742741f08736f612e474c20a4fcca32e4
Anylei <4nylei@gmail.com>
2014-12-17 07:00:39 +0800

    comment warning lines

--- MJRefreshExample/MJRefreshExample/Classes/Controller/MJTableViewController.m
+++ MJRefreshExample/MJRefreshExample/Classes/Controller/MJTableViewController.m
@@ -73,7 +73,7 @@ NSString *const MJTableViewCellIdentifier = @"Cell";
 //    [self.tableView addHeaderWithTarget:self action:@selector(headerRereshing)];
     // dateKey用于存储刷新时间，可以保证不同界面拥有不同的刷新时间
     [self.tableView addHeaderWithTarget:self action:@selector(headerRereshing)];
-#warning 自动刷新(一进入程序就下拉刷新)
+//#warning 自动刷新(一进入程序就下拉刷新)
     [self.tableView headerBeginRefreshing];
     
     // 2.上拉加载更多(进入刷新状态就会调用self的footerRereshing)
